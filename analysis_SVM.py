4af732d5a384a9e421352ba850cf49e61781604d
joonhainf <joonha1998@gmail.com>
2022-12-02 22:49:06 -0500

    SVM 2

--- analysis_SVM.py
+++ analysis_SVM.py
@@ -14,14 +14,17 @@ import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
+from matplotlib.colors import ListedColormap
+from sklearn.preprocessing import LabelEncoder
+from sklearn.datasets import make_blobs
+from sklearn import svm, datasets
 
 
-
-df = pd.read_table("steam_games.csv", delimiter = ";", low_memory=False, nrows = 10000)
+df = pd.read_table("steam_games.csv", delimiter = ";", low_memory=False, nrows = 1000)
 
 ##Create a binary variable which will be 0 if there is no discount and 1 if there is any discount
 
-df['Has_Discount'] = [0 if x==0 else 1  for x in df['Discount']]
+df['Has_Discount'] = ['Yes' if x==0 else 'No'  for x in df['Discount']]
 
 df.insert( "Has_Discount",df.["Discount"], True)
 
@@ -51,27 +54,15 @@ cm = confusion_matrix(Y_test,Y_pred)
 accuracy = cm.diagonal().sum()/len(Y_test)
 print("\nSVM Accuracy : ", accuracy)
 
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
-Y_train = le.fit_transform(Y_train)
+##Linear SVM with the purpose of showing the support vectors vs the training data
+classifier1 = SVC(kernel = 'linear', C=10, gamma=10)
+classifier1.fit(X_train,Y_train)
 
-from sklearn.svm import SVC
-classifier = SVC(kernel='rbf', random_state = 1)
-classifier.fit(X_train,Y_train)
+support_vectors = classifier1.support_vectors_
 
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-plt.figure(figsize = (7,7))
-X_set, y_set = X_train, Y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01), np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.75, cmap = ListedColormap(('black', 'white')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'orange'))(i), label = j)
-plt.title('Apples Vs Oranges')
-plt.xlabel('Weight In Grams')
-plt.ylabel('Size in cm')
-plt.legend()
+plt.scatter(X_train[:,0], X_train[:,1])
+plt.scatter(support_vectors[:,0], support_vectors[:,1], color='red')
+plt.title('Linearly separable data with support vectors')
+plt.xlabel('Negative Reviews')
+plt.ylabel('Initial Price')
 plt.show()
